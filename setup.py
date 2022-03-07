41c378a373a888f27b8e5995c2eee036f169cac2
V. A. Canales L <victorcanlima@gmail.com>
2022-03-07 14:46:54 -0600

    Initialized Cookiecutter Data Science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Case Study: Cycle Sharing Scheme — Determining Brand Persona from Python Machine Learning Case Studies by Danish Haroon',
+    author='Victor A. Canales-Lima',
+    license='MIT',
+)
